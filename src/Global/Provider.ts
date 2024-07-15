3deac0dfb2edcbb9485ce362d082b1eb35a68cbf
mrpiper21 <benbaah104@gmail.com>
2024-07-15 19:28:40 +0000

    update

--- src/Global/Provider.ts
+++
@@ -1,8 +0,0 @@
-// import { useUserContext } from "../hooks/Usercontext/UserContext";
-
-// type ProviderComponent = React.JSXElementConstructor<React.PropsWithChildren<{}>>;
-
-// // Define the BuildProviderTree function with types
-// function BuildProviderTree<ProviderComponent extends React.JSXElementConstructor<React.PropsWithChildren<{}>>>(providers: ProviderComponent[]): React.ReactNode {
-//   return providers.reduceRight((acc: React.ReactNode, Parent) => <Parent {...acc}>, null);
-// }
